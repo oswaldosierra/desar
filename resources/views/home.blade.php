cff678f85334b5f9f119514b5e88436ce5a9b704
Oswaldo Sierra <oswaldox199@gmail.com>
2025-02-07 09:45:49 -0500

    Actualiza configuración de NativePHP y mejora la vista principal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{ asset('icon.png') }}" type="image/png">
 
     <title>DESCAR</title>
 
@@ -18,7 +19,7 @@
     <div class="w-screen h-screen flex justify-center items-center flex-col">
         <h1 class="text-3xl mb-3">DESCAR</h1>
         <form action="{{ route('home') }}" method="post" id="form"
-            class="card p-4 flex flex-col border border-gray-200 rounded-lg w-5/12">
+            class="card p-4 flex flex-col border border-gray-200 rounded-lg w-10/12">
             @csrf
             <input type="text" name="url" class="input input-bordered w-full" placeholder="Ingrese Url">
             <select name="type" class="select select-bordered">
