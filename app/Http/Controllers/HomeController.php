609e75329b107c3dae2077bff31c2629aaffa7f9
Oswaldo Sierra <oswaldox199@gmail.com>
2025-02-09 12:56:40 -0500

    Ya funciona en win

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,19 +25,33 @@ class HomeController extends Controller
         $url = $request->input('url');
         $type = $request->input('type');
 
-        $dir = Dialog::new()->title('Donde guardar los archvios?')->folders()->open();
+        $dir = Dialog::new()->title('¿Dónde guardar los archivos?')->folders()->open();
         if (!$dir) return redirect()->route('home');
 
+        $binPath = __DIR__ . "/../../bin";
+        $isWindows = PHP_OS_FAMILY === 'Windows';
+
+        $pythonExists = shell_exec(($isWindows ? "where python" : "which python3") . " 2>&1");
+        if (!$pythonExists) {
+            if ($isWindows) {
+                $pythonZipPath = "$binPath/python.zip";
+                $pythonExtractPath = "$binPath/python";
+                shell_exec("powershell -command \"Expand-Archive -Path '$pythonZipPath' -DestinationPath '$pythonExtractPath' -Force\"");
+            } else {
+                shell_exec("sudo apt update && sudo apt install -y python3 || sudo yum install -y python3 || brew install python3");
+            }
+        }
+
         $yt = new YoutubeDl();
-        $yt->setPythonPath(__DIR__ . "/../../bin/lin/python3.10");
-        $yt->setBinPath(__DIR__ . "/../../bin/lin/yt-dlp");
+        if ($isWindows && !$pythonExists) $yt->setPythonPath("$binPath/python/python.exe");
+        $yt->setBinPath($isWindows ? "$binPath/win/yt-dlp.exe" : "$binPath/lin/yt-dlp");
 
         $Options = Options::create()->downloadPath($dir)->url($url)->output('%(title)s.%(ext)s');
         if ($type == 'mp3') $Options = $Options->format('bestaudio/best')->extractAudio(true)->audioFormat('mp3');
 
         $yt->download($Options);
 
-        Notification::title('Descarga Completada')->message('El archivo se ha descargado con exito')->show();
+        Notification::title('Descarga Completada')->message('El archivo se ha descargado con éxito')->show();
         return redirect()->route('home');
     }
 }
